9ceb9d5ed1a612246930b72de2a0d1674441f569
Youssef04544 <youssefbenyedder04@gmail.com>
2022-06-29 17:55:36 +0100

    Make playerselection and computer selection functionality

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -1,1 +1,16 @@
-console.log("welcome to this fun game");
+const SIGNS = ["rock", "paper", "scissors"];
+
+function playRound() {
+  let playerSelection = "";
+  while (!SIGNS.includes(playerSelection)) {
+    playerSelection = prompt("rock, paper or scissors?").toLowerCase();
+  }
+  const computerSelection = computerPlay();
+  return `${playerSelection} ${computerSelection}`;
+}
+
+const computerPlay = () => {
+  return SIGNS[Math.floor(Math.random() * 3)];
+};
+
+console.log(playRound());
