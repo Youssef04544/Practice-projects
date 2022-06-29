6aa58565e27e08ee1ca2f40d884579d7cbfddbb7
Youssef04544 <youssefbenyedder04@gmail.com>
2022-06-29 18:39:37 +0100

    add primary winner checking

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -6,11 +6,29 @@ function playRound() {
     playerSelection = prompt("rock, paper or scissors?").toLowerCase();
   }
   const computerSelection = computerPlay();
-  return `${playerSelection} ${computerSelection}`;
+
+  return checkPlayerWinner(playerSelection, computerSelection);
 }
 
 const computerPlay = () => {
   return SIGNS[Math.floor(Math.random() * 3)];
+};
+
+const checkPlayerWinner = (choice1, choice2) => {
+  if (choice1 === choice2) {
+    return "it's a tie";
+  } else {
+    if (choice1 === "rock") {
+      if (choice2 === "paper") return "player looses";
+      else return "player wins";
+    } else if (choice1 === "paper") {
+      if (choice2 === "scissors") return "player looses";
+      else return "player wins";
+    } else if (choice1 === "scissors") {
+      if (choice2 === "rock") return "player looses";
+      else return "player wins";
+    }
+  }
 };
 
 console.log(playRound());
